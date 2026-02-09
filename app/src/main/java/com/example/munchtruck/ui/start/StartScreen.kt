40ac9e24325d9c533f03e53c10dcad0aad6727ad
Linnéa <linnea.ternevik@gmail.com>
2026-02-09 23:39:13 +0100

    Added MunchTruck logo and updated start screen layout

--- app/src/main/java/com/example/munchtruck/ui/start/StartScreen.kt
+++ app/src/main/java/com/example/munchtruck/ui/start/StartScreen.kt
@@ -17,9 +17,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.munchtruck.R
 import com.example.munchtruck.ui.theme.PrimaryOrange
@@ -40,15 +46,36 @@ fun StartScreen(navController: NavController) {
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(24.dp),
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            Spacer(modifier = Modifier.height(230.dp))
+
             Text(
-                text = "Welcome to MunchTruck",
-                style = MaterialTheme.typography.headlineMedium
+                text = "Welcome to",
+                style = MaterialTheme.typography.headlineLarge.copy(
+                    fontSize = 50.sp,
+                    shadow = Shadow(
+                        color = Color.Black.copy(alpha = 0.9f),
+                        offset = Offset(4f, 4f),
+                        blurRadius = 10f
+                    )
+                ),
+
+                color = White,
+                fontWeight = FontWeight.Bold
+            )
+//
+            Image(
+                painter = painterResource(id = R.drawable.munchtruck_text),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(70.dp)
             )
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(16.dp))
             Button(
                 onClick = {
                     navController.navigate("login")
