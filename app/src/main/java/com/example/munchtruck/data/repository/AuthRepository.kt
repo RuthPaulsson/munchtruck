c25e7376985898b21a37af40c45edac4a6ef2145
PatNoO <Noordh94@hotmail.se>
2026-02-10 08:43:01 +0100

    created function för register user -> firebase.firestore

--- app/src/main/java/com/example/munchtruck/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/munchtruck/data/repository/AuthRepository.kt
@@ -1,14 +1,39 @@
 package com.example.munchtruck.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 class AuthRepository {
     private val firebaseAuth = FirebaseAuth.getInstance()
-
+    private val firestore = FirebaseFirestore.getInstance()
+    
     suspend fun login(email: String, password: String) {
         try {
             firebaseAuth.signInWithEmailAndPassword(email, password).await()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+
+    suspend fun register(email: String, password: String) {
+        try {
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+        val uid = firebaseAuth.currentUser?.uid ?: throw Exception("User not found")
+
+        val user = hashMapOf(
+            "uid" to uid,
+            "email" to email
+        )
+
+        firestore.collection(
+            "users")
+            .document(uid)
+            .set(user)
+            .await()
+
         } catch (e: Exception) {
             throw e
         }
