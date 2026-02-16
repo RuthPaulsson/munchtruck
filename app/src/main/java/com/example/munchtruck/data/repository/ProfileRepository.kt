dd5106bd297c4dca8205b178ad5a5bfb64527c3d
PatNoO <Noordh94@hotmail.se>
2026-02-16 15:59:59 +0100

    Added Repository interface for truck profile managment

--- app/src/main/java/com/example/munchtruck/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/munchtruck/data/repository/ProfileRepository.kt
@@ -1,4 +1,17 @@
 package com.example.munchtruck.data.repository
 
-class ProfileRepository {
+import com.example.munchtruck.data.model.FoodTruck
+
+interface ProfileRepository {
+    suspend fun getTruckProfile (ownerUid: String): FoodTruck
+
+    suspend fun updateTruckProfile (
+        ownerUid: String,
+        name: String,
+        description: String,
+        location: String,
+        isOpen: Boolean,
+        imageUrl: String
+    ): FoodTruck
+
 }
