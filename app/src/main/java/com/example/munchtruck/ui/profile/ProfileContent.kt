ec6c5f98d493cedc96a986a3b9907ba54a3602b7
Linnéa <linnea.ternevik@gmail.com>
2026-02-19 16:29:46 +0100

    Added ProfileHeroSection to ProfileContent

--- app/src/main/java/com/example/munchtruck/ui/profile/ProfileContent.kt
+++ app/src/main/java/com/example/munchtruck/ui/profile/ProfileContent.kt
@@ -1,20 +1,30 @@
 package com.example.munchtruck.ui.profile
 
 import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.munchtruck.R
 import com.example.munchtruck.ui.theme.AppPreviewWrapper
 import com.example.munchtruck.ui.theme.Dimens.SpaceL
 
@@ -28,18 +38,17 @@ fun ProfileContent (
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
-        Spacer(modifier = Modifier.height(SpaceL))
-
-        Text("Truck name")
+        ProfileHeroSection (onEditClick)
 
         Spacer(modifier = Modifier.height(SpaceL))
 
-        Button(
-            onClick = onEditClick,
-            modifier = Modifier.align(Alignment.Start)
-        ) {
-            Text("Edit Profile")
-        }
+        Text(
+            text ="About section coming next...",
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(SpaceL))
+
 
         Spacer(modifier = Modifier.height(SpaceL))
         Button(
@@ -51,6 +60,41 @@ fun ProfileContent (
         Spacer(modifier = Modifier.height(SpaceL))
     }
 }
+@Composable
+fun ProfileHeroSection(
+    onEditClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(220.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(0.dp))
+        )
+        TextButton(
+            onClick = onEditClick,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+        ) {
+            Text("Edit")
+        }
+
+        Text(
+            text = "Truck Name",
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
