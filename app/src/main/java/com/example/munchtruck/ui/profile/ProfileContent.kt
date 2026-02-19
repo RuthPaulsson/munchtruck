c032a4685a6dfce8ee2f446e7323721fbba6c2c8
Linnéa <linnea.ternevik@gmail.com>
2026-02-19 15:52:35 +0100

    Refactored ProfileContent to scrollable column layout

--- app/src/main/java/com/example/munchtruck/ui/profile/ProfileContent.kt
+++ app/src/main/java/com/example/munchtruck/ui/profile/ProfileContent.kt
@@ -1,15 +1,19 @@
 package com.example.munchtruck.ui.profile
 
+import android.widget.Space
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.munchtruck.ui.theme.AppPreviewWrapper
 import com.example.munchtruck.ui.theme.Dimens.SpaceL
@@ -19,32 +23,32 @@ fun ProfileContent (
     onLogoutClick: () -> Unit,
     onEditClick: () -> Unit
 ) {
-    // ====== Layout ===============================
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(SpaceL))
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Truck name")
 
-            Text("Welcome!")
+        Spacer(modifier = Modifier.height(SpaceL))
 
-            Spacer(modifier = Modifier.height(SpaceL))
-
-            Button(
-                onClick = onEditClick
-            ) {
-                Text(("Edit profile"))
-            }
-
-            Spacer(modifier = Modifier.height(SpaceL))
-
-            Button(
-                onClick = onLogoutClick
-            ) {
-                Text("Log out")
-            }
+        Button(
+            onClick = onEditClick,
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Text("Edit Profile")
         }
+
+        Spacer(modifier = Modifier.height(SpaceL))
+        Button(
+            onClick = onLogoutClick,
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Text("Log out")
+        }
+        Spacer(modifier = Modifier.height(SpaceL))
     }
 }
 
