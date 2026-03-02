abb0d777136719291ad1e3718590252f0ff60213
Ahmad <ahmadalhayel20@gmail.com>
2026-03-02 18:26:27 +0100

    Feat: Implement reauthenticate and deletion logic in FirebaseAuthRepository

--- app/src/main/java/com/example/munchtruck/data/firebase/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/munchtruck/data/firebase/FirebaseAuthRepository.kt
@@ -2,22 +2,20 @@ package com.example.munchtruck.data.firebase
 
 import com.example.munchtruck.data.FirestoreFields
 import com.example.munchtruck.data.repository.AuthRepository
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
-
 
 class FirebaseAuthRepository(
     private val firebaseAuth: FirebaseAuth,
     private val firestore: FirebaseFirestore
-): AuthRepository {
-
-
+) : AuthRepository {
 
     override suspend fun login(email: String, password: String) {
         firebaseAuth.signInWithEmailAndPassword(email, password).await()
     }
-
 
     override suspend fun register(email: String, password: String) {
         val authResult = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
@@ -46,16 +44,39 @@ class FirebaseAuthRepository(
             batch.set(userRef, user)
             batch.set(truckRef, truck)
         }.await()
-
-
     }
 
     override fun logout() {
         firebaseAuth.signOut()
     }
 
-
     override fun isUserLoggedIn(): Boolean {
         return firebaseAuth.currentUser != null
+    }
+
+    override suspend fun reauthenticate(password: String) {
+        val user = firebaseAuth.currentUser ?: throw Exception("No user logged in")
+        val email = user.email ?: throw Exception("User email is missing.")
+
+        val credential = EmailAuthProvider.getCredential(email, password)
+        user.reauthenticate(credential).await()
+    }
+
+
+    override suspend fun deleteUserDocument() {
+        val uid = firebaseAuth.currentUser?.uid ?: throw Exception("UID is missing")
+        firestore.collection("users").document(uid).delete().await()
+    }
+
+
+    override suspend fun deleteAuthAccount() {
+        val user = firebaseAuth.currentUser ?: throw Exception("No active session")
+        try {
+            user.delete().await()
+        } catch (e: FirebaseAuthRecentLoginRequiredException) {
+            throw e
+        } catch (e: Exception) {
+            throw Exception("No active session ${e.message}")
+        }
     }
 }
